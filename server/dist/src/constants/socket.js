27a7d9cc5cbde831f798801285b898e508d8c6e9
asherLZR <asher.lzrz@gmail.com>
2020-09-07 21:32:45 +1000

    Build dist on heroku [skip netlify]

--- server/dist/src/constants/socket.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SocketTypes = void 0;
-var SocketTypes;
-(function (SocketTypes) {
-    SocketTypes["JOIN"] = "JOIN";
-    SocketTypes["LEAVE"] = "LEAVE";
-})(SocketTypes = exports.SocketTypes || (exports.SocketTypes = {}));
-//# sourceMappingURL=socket.js.map
--- server/dist/src/types/TileTypes.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BLANK_CHAR = void 0;
-exports.BLANK_CHAR = 'blank';
-//# sourceMappingURL=TileTypes.js.map
